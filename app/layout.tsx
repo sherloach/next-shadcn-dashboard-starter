6ce45ac2ac445ac10f17542c1c8e50908e1f108a
Hayden Ngo <hoango1607@gmail.com>
2024-07-02 21:15:59 +0930

    chore: setup eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import '@uploadthing/react/styles.css';
+import './globals.css';
+
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import NextTopLoader from 'nextjs-toploader';
+
+import { auth } from '@/auth';
 import Providers from '@/components/layout/providers';
 import { Toaster } from '@/components/ui/toaster';
-import '@uploadthing/react/styles.css';
-import type { Metadata } from 'next';
-import NextTopLoader from 'nextjs-toploader';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { auth } from '@/auth';
 
 const inter = Inter({ subsets: ['latin'] });
 
